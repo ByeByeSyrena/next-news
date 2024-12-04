021e0b3c245664136e28ffabd26d1d34b09e7c73
Ira S <iryna.shevchenko2@gmail.com>
2024-12-04 19:56:05 +0200

    filter commit doesnt work I think because I changed code but np

--- app/(content)/archive/@archive/[[...filter]]/page.js
+++ app/(content)/archive/@archive/[[...filter]]/page.js
@@ -3,6 +3,36 @@ import { getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsF
 import Link from "next/link";
 import { Suspense } from "react";
 
+async function FilterHeader ({year, month, filter}){
+  let links = getAvailableNewsYears();
+
+  if (filter && year && !month) {
+    links = getAvailableNewsMonths(year);
+  }
+
+  if (filter && year && month) {
+    links = [];
+  }
+
+  return (<header id='archive-header'>
+      <nav>
+        <ul>
+          {links.map(link => {
+            const href = year ?
+              `/archive/${year}/${link}` :
+              `/archive/${link}`;
+
+            return (<li key={link}>
+              <Link href={href}>{link}</Link>
+            </li>
+            )
+          })}
+        </ul>
+      </nav>
+    </header>
+    );
+}
+
 async function FilteredNews ({year, month}){
   let news;
 
@@ -28,16 +58,6 @@ export default async function FilteredNewsPage({ params }) {
   const selectedYear = filter?.[0];
   const selectedMonth = filter?.[1];
 
-  let links = getAvailableNewsYears();
-
-  if (filter && selectedYear && !selectedMonth) {
-    links = getAvailableNewsMonths(selectedYear);
-  }
-
-  if (filter && selectedYear && selectedMonth) {
-    links = [];
-  }
-
 
   if (
     (selectedYear && !getAvailableNewsYears().includes(selectedYear)) ||
@@ -47,22 +67,9 @@ export default async function FilteredNewsPage({ params }) {
   }
 
   return <>
-    <header id='archive-header'>
-      <nav>
-        <ul>
-          {links.map(link => {
-            const href = selectedYear ?
-              `/archive/${selectedYear}/${link}` :
-              `/archive/${link}`;
-
-            return (<li key={link}>
-              <Link href={href}>{link}</Link>
-            </li>
-            )
-          })}
-        </ul>
-      </nav>
-    </header>
+  <Suspense fallback={<p>Loading filter...</p>}>
+    <FilterHeader year={selectedYear} month={selectedMonth} filter={filter}/>
+    </Suspense>
     <Suspense fallback={<p>Loading news...</p>}>
     <FilteredNews year={selectedYear} month={selectedMonth}/>
     </Suspense>
